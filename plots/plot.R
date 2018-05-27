4e78188fcc4de7ba2c3e02286b0bc030927062ef
Dhruv <ds3638@columbia.edu>
2018-05-27 13:04:47 +0530

    shifting to GPS approach

--- plots/plot.R
+++ plots/plot.R
@@ -1,12 +1,10 @@
-tbl <- read.table("final1.3.Q")
-sampleFam <- read.csv("sample.fam", sep=" ", header=F)
-finalFam <- read.csv("final1.fam", sep=" ", header=F)
+tbl <- read.table("merge.9.Q")
 
-sampleRow <- which(grepl(sampleFam[1,1], finalFam[,1]))
+sampleRow <- 1
 
 cat(toString(tbl[sampleRow,]), file='ancestry.txt') 
 png(file="ancestry.png")
 slices <- as.numeric(tbl[sampleRow,])
-lbls <- c("Central Europe", "East Africa", "East Asia")
+lbls <- c("Med", "Nat", "NEA", "NEU", "OCE", "RSA", "SEA", "SWA", "SSA")
 pie(slices, labels = lbls, main="Ancestry Chart")
 dev.off()

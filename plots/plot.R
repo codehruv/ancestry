9540abcfd0de71a777739f82268bb02815c75dee
Dhruv <ds3638@columbia.edu>
2018-05-27 09:24:47 +0530

    plot updated for pie chart

--- plots/plot.R
+++ plots/plot.R
@@ -3,21 +3,10 @@ sampleFam <- read.csv("sample.fam", sep=" ", header=F)
 finalFam <- read.csv("final1.fam", sep=" ", header=F)
 
 sampleRow <- which(grepl(sampleFam[1,1], finalFam[,1]))
-people <- c()
 
-temp <- tbl[1,]
-tbl[1,] <- tbl[sampleRow,]
-tbl[sampleRow,] <- temp
-
-for(i in 1:180) {
-    people[i] = ""
-    if( i == 1 ) people[i] = "YOU"
-    if( i == 61 ) people[i] = "CEU"
-    if( i == 120 ) people[i] = "YRI"
-    if( i == 180 ) people[i] = "HCBJPT"
-}
-
-cat(toString(tbl[1,]), file='ancestry.txt') 
+cat(toString(tbl[sampleRow,]), file='ancestry.txt') 
 png(file="ancestry.png")
-barplot( t(as.matrix(tbl)), col=rainbow(3), names.arg=people, las=2, cex.names=0.5, xlab="People", ylab="Ancestry", border=NA )
+slices <- as.numeric(tbl[sampleRow,])
+lbls <- c("Central Europe", "East Africa", "East Asia")
+pie(slices, labels = lbls, main="Ancestry Chart")
 dev.off()

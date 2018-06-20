a9805368c5bde081487a4bd5008d59706e639cc9
Dhruv Singh <omnipotentdhruv@Dhruvs-MacBook-Pro.local>
2018-06-20 19:55:36 -0400

    fixed pie chart labelling

--- plots/plot.R
+++ plots/plot.R
@@ -5,9 +5,13 @@ finalFam <- read.csv("merge.fam", sep=" ", header=F)
 
 sampleRow <- which(grepl("FAM001", finalFam[,1]))
 
+labels <- unique(finalFam[,1])
+lbls <- lbls[lbls != "FAM001"]
+
+lbls
+
 cat(toString(tbl[sampleRow,]), file='ancestry.txt') 
 png(file="ancestry.png")
 slices <- as.numeric(tbl[sampleRow,])
-lbls <- c("East Asian", "European", "African")
 pie(slices, labels = lbls, main="Ancestry Chart")
 dev.off()

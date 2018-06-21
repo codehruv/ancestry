5647d3cff017be2acf677f7534c2ce2af61c69ca
Dhruv Singh <omnipotentdhruv@Dhruvs-MacBook-Pro.local>
2018-06-20 20:08:57 -0400

    temporary fix works if all populations pass merge

--- plots/plot.R
+++ plots/plot.R
@@ -5,13 +5,9 @@ finalFam <- read.csv("merge.fam", sep=" ", header=F)
 
 sampleRow <- which(grepl("FAM001", finalFam[,1]))
 
-labels <- unique(finalFam[,1])
-lbls <- lbls[lbls != "FAM001"]
-
-lbls
-
 cat(toString(tbl[sampleRow,]), file='ancestry.txt') 
 png(file="ancestry.png")
 slices <- as.numeric(tbl[sampleRow,])
+lbls <- c("African", "European", "East Asian")
 pie(slices, labels = lbls, main="Ancestry Chart")
 dev.off()

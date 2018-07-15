5cc4bf43391d13426e70b2e3cc1c3877621ca04b
Dhruv Singh <omnipotentdhruv@Dhruvs-MacBook-Pro.local>
2018-07-15 18:03:53 -0400

    rehaul

--- plots/plot.R
+++ plots/plot.R
@@ -8,7 +8,10 @@ sampleRow <- which(grepl(" ", popsFam[,1]))
 cat(toString(tbl[sampleRow,]), file='ancestry.txt') 
 png(file="ancestry.png")
 slices <- as.numeric(tbl[sampleRow,])
-lbls <- c("African", "East Asian", "European", "MiddleEastern", "South Asian")
+lbls <- unique(popsFam)
+lbls <- lbls[!(lbls == " ")]
+ 
+#lbls <- c("African", "East Asian", "European", "MiddleEastern","South Asian")
 #lbls <- c("ACB", "ASW", "CEU", "CLM", "MXL", "NAT", "PEL", "PUR", "YRI")
 pie(slices, labels = lbls, main="Ancestry Chart")
 dev.off()

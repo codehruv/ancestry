e3a0ee6f896bc131051e0cdbc66b4e66ae8d67a0
Dhruv Singh <omnipotentdhruv@Dhruvs-MacBook-Pro.local>
2018-08-05 18:48:54 -0400

    commented and clarified code

--- plots/plot.R
+++ plots/plot.R
@@ -1,17 +1,41 @@
+# Step 1: Input
 args <- commandArgs(TRUE)
-
 tbl <- read.table(args[1])
 
 popsFam <- read.csv("merge.pop", sep="\n", header=F)
-sampleRow <- which(grepl(" ", popsFam[,1]))
 
-cat(toString(tbl[sampleRow,]), file='ancestry.txt') 
-png(file="ancestry.png")
+# Step 2: Extracting Result
+# 2.1 Numerical Result
+sampleRow <- which(grepl(" ", popsFam[,1]))
 slices <- as.numeric(tbl[sampleRow,])
+
+# 2.2 Corresponding populations
 lbls <- unique(popsFam)
 lbls <- lbls[!(lbls == " ")]
- 
-#lbls <- c("African", "East Asian", "European", "MiddleEastern","South Asian")
-#lbls <- c("ACB", "ASW", "CEU", "CLM", "MXL", "NAT", "PEL", "PUR", "YRI")
-pie(slices, labels = lbls, main="Ancestry Chart")
+
+
+# Step 3: Clearing up results and plotting
+# 3.1 Clearing up results
+final_table <- data.frame(slices, lbls)
+final_table <- final_table[final_table$slices > 0.00010, ]
+
+# 3.2 Plotting
+png(file="ancestry.png")
+pie(final_table[,1], labels = final_table[,2], main="Ancestry Chart")
 dev.off()
+
+
+# Later development
+if(FALSE) {
+pop2region <- read.table("pops2region.txt", sep=" ", header=F)
+names(pop2region) <- c("Pop", "Region")
+
+finalized_table <- merge(final_table, pop2region, by.x='lbls', by.y='Pop')
+
+#finalized_table <- finalized_table[finalized_table$slices > 0.00010, ]
+
+print(finalized_table)
+print(final_table)
+}
+
+
